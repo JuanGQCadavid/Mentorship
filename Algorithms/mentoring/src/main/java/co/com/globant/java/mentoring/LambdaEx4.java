29a7caef2a99f1f1953a880854bdc58162da177d
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-02-14 14:41:40 -0500

    Lambda 4 Done

--- Algorithms/mentoring/src/main/java/co/com/globant/java/mentoring/LambdaEx4.java
+++ Algorithms/mentoring/src/main/java/co/com/globant/java/mentoring/LambdaEx4.java
@@ -1,6 +1,8 @@
 package co.com.globant.java.mentoring;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,27 +15,54 @@ import java.util.stream.Collectors;
  * Organize assiduously.
  */
 public class LambdaEx4 {
-    final int [] NUMBERS ={2,4,6,8,3,5,7,9,11};
+    final Integer [] NUMBERS ={3,5,7,9,11, 2,4,6,8,};
 
     public LambdaEx4(){}
 
     public boolean evenFunction(int number){
         return (number % 2) == 0 ? true: false;
     }
+    public boolean oddFunction(int number){
+        return (number % 2) == 0 ? false: true;
+    }
 
+    public void detachanlyArraged(){
+        List results = Arrays.stream(NUMBERS)
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println("detachanlyArraged -> " + results);
+    }
+
+    public void assiduouslyArraged(){
+        List results = Arrays.stream(NUMBERS)
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
+        System.out.println("assiduouslyArraged -> " + results);
+    }
     public void evenNumbers(){
         List results = Arrays.stream(NUMBERS)
                 .filter(number -> this.evenFunction(number))
                 .collect(Collectors.toList());
-        System.out.println("Partial result -> " + results);
+
+        System.out.println("even Numbers -> " + results);
     }
 
-    public void oddNumber(){
+    public void oddNumbers(){
+        List results = Arrays.stream(NUMBERS)
+                .filter(number -> this.oddFunction(number))
+                .collect(Collectors.toList());
+        System.out.println("Odd Numbers -> " + results);
     }
+
+
 
     public static void main(String[] args) {
         LambdaEx4 lm4 = new LambdaEx4();
         lm4.evenNumbers();
+        lm4.oddNumbers();
+        lm4.detachanlyArraged();
+        lm4.assiduouslyArraged();
+
     }
 
 }
